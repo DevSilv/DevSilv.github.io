ce87e63a00b43550bb0198f49caabfa4bdf7687c
silvuss <silvuss@o2.pl>
2018-08-03 17:02:15 +0200

    change css with bootstrap

--- js/display-search-results.js
+++ js/display-search-results.js
@@ -1,10 +1,11 @@
 function displaySearchResults(noResultsText) {
-    const input = $(".search-form__input");
-    const resultsElement = $(".search-form__results-list");
+    const input = $("#search-form__input");
+    const resultsElement = $("#search-form__results-list");
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            resultsElement.empty();
             const data = this.response;
             const xml = new DOMParser().parseFromString(data, "text/xml");
             const results = [];
@@ -27,17 +28,18 @@ function displaySearchResults(noResultsText) {
                     const rLink = document.createElement("a");
                     rLink.setAttribute("href", r.link);
                     rLink.append(r.title);
-                    rLink.classList.add("search-form__results-list__item__link");
+                    rLink.classList.add("search-form__results-list__item__link", "text-light", "pt-1", "pb-1", "pl-2", "pr-2");
+                    $(rLink).css("display", "block");
                     const rElement = document.createElement("li");
                     rElement.append(rLink);
-                    rElement.classList.add("search-form__results-list__item");
+                    rElement.classList.add("list-group-item", "bg-primary", "border", "p-0");
                     resultsElement.append(rElement);
                 }
             } else {
                 const errorElement = document.createTextNode(noResultsText);
                 const rElement = document.createElement("li");
                 rElement.append(errorElement);
-                rElement.classList.add("search-form__results-list__item", "search-form__results-list__item--error");
+                rElement.classList.add("list-group-item", "bg-secondary", "text-light", "pt-1", "pb-1", "pl-2", "pr-2");
                 resultsElement.append(rElement);
             }
             resultsElement.show();
--- js/search.js
+++ js/search.js
@@ -2,13 +2,13 @@
 function hideResults(event) {
     if (event.relatedTarget === null ||
         !event.relatedTarget.classList.contains("search-form__results-list__item__link")) {
-        $(".search-form__results-list").hide();
+        $("#search-form__results-list").hide();
     }
 }
 
 // onfocus
 function showResults() {
-    const resultsElement = $(".search-form__results-list");
+    const resultsElement = $("#search-form__results-list");
     if (resultsElement.text() != "") {
         resultsElement.show();
     }
@@ -16,9 +16,8 @@ function showResults() {
 
 // onkeyup
 function search(noResultsText) {
-    const resultsElement = $(".search-form__results-list");
-    resultsElement.empty();
-    if ($(".search-form__input").val() != "") {
+    const resultsElement = $("#search-form__results-list");
+    if ($("#search-form__input").val() != "") {
         displaySearchResults(noResultsText);
     } else {
         resultsElement.hide();
